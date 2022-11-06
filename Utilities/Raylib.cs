4a586d15e50457a4a4685dd3d719b8216c3d615d
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2022-11-06 00:37:33 +0000

    Add shader helper class

--- Utilities/Raylib.cs
+++ Utilities/Raylib.cs
@@ -13,4 +13,24 @@ namespace PingPong.Utilities
         public static int OffsetX(int offset) => Raylib.GetScreenWidth() - offset;
         public static int OffsetY(int offset) => Raylib.GetScreenHeight() - offset;
     }
+
+    public static class Shaders
+    {
+        // set shader position
+        public static void SetValuePosition(Shader shader, int location, Vector2 position)
+        {
+            float[] positionArray = new float[2] { position.X, position.Y };
+
+            Raylib.SetShaderValue(shader, location, positionArray, ShaderUniformDataType.SHADER_UNIFORM_VEC2);
+        }
+
+        public static void SetValueColor(Shader shader, int location, Color color)
+        {
+            // Create a float array from the color
+            float[] colorArray = new float[] { color.r / 255f, color.g / 255f, color.b / 255f, color.a / 255f };
+
+            // Set shader value
+            Raylib.SetShaderValue(shader, location, colorArray, ShaderUniformDataType.SHADER_UNIFORM_VEC4);
+        }
+    }
 }
